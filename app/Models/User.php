811b7676b86fb76a402e483b7b68c7a2c533678e
Majd Jalab <majdjalab@gmail.com>
2024-08-07 16:09:49 +0200

    Database & Encrybtion added

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 
 class User extends Authenticatable
 {
@@ -44,4 +45,11 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    protected function password(): Attribute
+    {
+        return Attribute::make(
+            set: fn($value) => bcrypt($value)
+        );
+    }
+
 }
